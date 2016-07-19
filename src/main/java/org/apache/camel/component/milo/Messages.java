e730710ca07f5a7dba9547f8880d606b7249bfef
Jens Reimann <jreimann@redhat.com>
2016-07-19 15:32:16 +0200

    Improve documentation and JavaDoc

--- src/main/java/org/apache/camel/component/milo/Messages.java
+++ src/main/java/org/apache/camel/component/milo/Messages.java
@@ -23,6 +23,14 @@ public final class Messages {
 	private Messages() {
 	}
 
+	/**
+	 * Fill a Message from a DataValue
+	 * 
+	 * @param value
+	 *            the value to feed from
+	 * @param result
+	 *            the result to feed to
+	 */
 	public static void fillFromDataValue(final DataValue value, final DefaultMessage result) {
 		result.setBody(value);
 		result.setHeader("variantValue", value.getValue().getValue());
